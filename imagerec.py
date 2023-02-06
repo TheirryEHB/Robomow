e5b5b033c5672e546273c425171edf9a9cf4e669
ThierryKlougbo <80406706+TheirryEHB@users.noreply.github.com>
2023-02-06 15:21:22 +0100

    Shortest route1: BFS

--- imagerec.py
+++ imagerec.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 import time
 import serial
 import math
+import sys
+import queue
 
 print(cv2.__version__)
 
@@ -20,18 +22,6 @@ countCalc = 0
 tempFL = 645.3333333333334
 pingsPerRot = 3410
 
-
-###
-    # Matrix node class
-###
-    class QItem:
-        def __int__(self, row, col, dist):
-            self.row = row
-            self.col = col
-            self.dist = dist
-        def __repr__(self):
-            return f"QItem({self.row}, {self.col}, {self.dist})"
-
 arrayHoogte = []
 arrayBasis = []
 currentPosRob = [0, 0]
@@ -40,12 +30,117 @@ neighbours = []
 closestNeighbour = [1,1]
 weight = 0
 
-testHeight = [1,2,3,4,5,6]
-testBase = [1,2,3,4,5,6]
-testStart = QItem(3,2,0)
-testDest = QItem(4,4,0)
+testHeight = [0,1,2,3,4,5,6]
+testBase = [0,1,2,3,4,5,6]
+matrix = []
 
 
+
+###
+    # Make matrix
+###
+def makeMatrix():
+    for i in range(len(testBase)):
+        natr = []
+        for a in range(len(testHeight)):
+            natr.append([testBase[i],testHeight[a]])
+        matrix.append(natr)
+
+makeMatrix()
+matrix[3][4] = "*"
+
+#https://www.lavivienpost.com/shortest-path-between-cells-in-matrix-code/#:~:text=Shortest%20path%20in%20matrix%20is,starts%20from%20the%20source%20node.
+class Cell  :
+    def __init__(self, x, y, dist, prev) :
+        self.x = x
+        self.y = y
+        self.dist = dist; #distance to start
+        self.prev = prev; #parent cell in the path
+    def __str__(self):
+        return "("+ str(self.x) + "," + str(self.y) + ")" 
+    
+class ShortestPathBetweenCellsBFS :
+    #BFS, Time O(n^2), Space O(n^2)
+    def shortestPath(self, matrix, start, end) :
+        sx = start[0]
+        sy = start[1]
+        dx = end[0]
+        dy = end[1]
+		#if start or end value is 0, return
+        if matrix[sx][sy] == 0 or matrix[dx][dy] == 0 :
+            print("There is no path.")
+            return  
+		#initialize the cells 
+        m = len(matrix)
+        n = len(matrix[0])    
+        cells = []
+        for i in range (0, m) :
+            row = []
+            for j in range(0, n) :
+                row.append(Cell(i, j, sys.maxsize, None))
+                if matrix[i][j] != "*" :
+                    row.append(Cell(i, j, sys.maxsize, None))
+                else:
+                    row.append(None)
+            cells.append(row) 
+	    #breadth first search
+        queue = []     
+        src = cells[sx][sy]
+        src.dist = 0
+        queue.append(src)
+        dest = None
+        p = queue.pop(0)
+        while p != None :
+	    	#find destination 
+            if p.x == dx and p.y == dy : 
+                dest = p
+                break	             
+	        # moving up
+            self.visit(cells, queue, p.x-1, p.y, p)    
+            # moving left
+            self.visit(cells, queue, p.x, p.y-1, p)     
+	        # moving down
+            self.visit(cells, queue, p.x+1, p.y, p)             
+	        #moving right
+            self.visit(cells, queue, p.x, p.y+1, p)
+            if len(queue) > 0:
+                p = queue.pop(0)
+            else:
+                p = None       
+	    #compose the path if path exists
+        if dest == None :
+            print("there is no path.")
+            return
+        else :
+            path = []
+            p = dest
+            while p != None :
+                path.insert(0, p)	      
+                p = p.prev	       
+            for i in path:
+                print(i)
+	
+	#function to update cell visiting status, Time O(1), Space O(1)
+    def visit(self, cells, queue, x, y, parent) :		
+        #out of boundary
+        if x < 0 or x >= len(cells) or y < 0 or y >= len(cells[0]) or cells[x][y] == None :
+	        return
+	    #update distance, and previous node
+        dist = parent.dist + 1
+        p = cells[x][y]
+        if dist < p.dist :
+            p.dist = dist
+            p.prev = parent
+            queue.append(p)
+
+myObj = ShortestPathBetweenCellsBFS()   
+#case1, there is no path
+start = [3, 2]
+end = [4, 4]
+# print(matrix)
+print(matrix[0])
+print("case 1: ")
+myObj.shortestPath(matrix, start, end)
 
 
 ### 
@@ -128,50 +223,7 @@ def makeMatrixEven(ar1, ar2):
             ar1.append("*")
     weight = len(ar1)
 
-###
-    # Berekenen gewicht tussen matrix punten
-###
-def calcWeight(p1, p2):
-    dis = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-#     print(dis)
-    return dis
-# calcWeight([3,2], [2,1])
 
-def getNeighbours(p):
-    if(len(testBase) > p[0]+1):
-        nR = [p[0]+1, p[1]]
-        neighbours.append(nR)
-    if(p[0] > 0):
-        nL = [p[0]-1, p[1]]
-        neighbours.append(nL)
-    if(len(testHeight) > p[1]+1):
-        nU = [p[0], p[1]+1]
-        neighbours.append(nU)
-    if(p[1] > 0):
-        nD = [p[0], p[1]-1]
-        neighbours.append(nD)
-    getClosestNeighbour()
-#     print(neighbours)
-
-def getClosestNeighbour():
-    global closestNeighbour
-    global nextPos
-    for n in neighbours:
-        if(calcWeight(nextPos, n) < weight):
-            closestNeighbour = n
-        print(n)    
-        nextPos = closestNeighbour
-        shortestRout.append(closestNeighbour)
-#     print(shortestRout)
-
-
-# getNeighbours([6,0])
-nextPos = testStart
-def myDijkstra():
-    while(nextPos[0] != testDest[1] and nextPos[1] != testDest[1]):
-        getNeighbours(nextPos)
-
-myDijkstra()
 
 ###
     # Beeldherkenning gedeelte.
