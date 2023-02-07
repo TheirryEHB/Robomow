52393b281fd9df4c934b0a8480eea7ed969ab3f2
ThierryKlougbo <80406706+TheirryEHB@users.noreply.github.com>
2023-02-07 13:40:57 +0100

    First draft compass

--- imagerec.py
+++ imagerec.py
@@ -22,6 +22,7 @@ countCalc = 0
 tempFL = 645.3333333333334
 pingsPerRot = 3410
 
+## Pathfinding
 arrayHoogte = []
 arrayBasis = []
 currentPosRob = [0, 0]
@@ -30,11 +31,28 @@ neighbours = []
 closestNeighbour = [1,1]
 weight = 0
 
+## Orientation
+currOrientation = 0
+
+
 testHeight = [0,1,2,3,4,5,6]
 testBase = [0,1,2,3,4,5,6]
 matrix = []
 
-
+def changeOrLeft(degree):
+#     if(currOrientatiion+degree <= 180):
+#         currOrientation = currOrientation + degree
+#     if(currOrientatiion+degree > 180):
+#         currOrientatiion = -90 - degree
+    if(currOrientation + degree < 360):
+        currOrientatiion = currOrientatiion + degree
+    if(currOrientatiion + degree >= 360):
+        currOrientatiion = degree - (360 - currOrientatiion)
+def changeOrRight(degree):
+    if(currOrientatiion - degree >= 0):
+        currOrientatiion = currOrientatiion - degree
+    if(currOrientatiion - degree < 0):
+        currOrientatiion = 360 - (degree - currOrientatiion)
 
 ###
     # Make matrix
@@ -47,7 +65,7 @@ def makeMatrix():
         matrix.append(natr)
 
 makeMatrix()
-matrix[3][4] = "*"
+# matrix[3][4] = "*"
 
 #https://www.lavivienpost.com/shortest-path-between-cells-in-matrix-code/#:~:text=Shortest%20path%20in%20matrix%20is,starts%20from%20the%20source%20node.
 class Cell  :
@@ -66,11 +84,11 @@ class ShortestPathBetweenCellsBFS :
         sy = start[1]
         dx = end[0]
         dy = end[1]
-		#if start or end value is 0, return
+    #if start or end value is 0, return
         if matrix[sx][sy] == 0 or matrix[dx][dy] == 0 :
             print("There is no path.")
             return  
-		#initialize the cells 
+    #initialize the cells 
         m = len(matrix)
         n = len(matrix[0])    
         cells = []
@@ -82,8 +100,9 @@ class ShortestPathBetweenCellsBFS :
                     row.append(Cell(i, j, sys.maxsize, None))
                 else:
                     row.append(None)
+#                     row.append(myObj.shortestPath(matrix, [row[-1].x, row[-1].y], end))
             cells.append(row) 
-	    #breadth first search
+        #breadth first search
         queue = []     
         src = cells[sx][sy]
         src.dist = 0
@@ -91,23 +110,24 @@ class ShortestPathBetweenCellsBFS :
         dest = None
         p = queue.pop(0)
         while p != None :
-	    	#find destination 
+           #find destination 
             if p.x == dx and p.y == dy : 
                 dest = p
-                break	             
-	        # moving up
+                break           
+            #moving right
+            self.visit(cells, queue, p.x, p.y+1, p)
+            # moving up
             self.visit(cells, queue, p.x-1, p.y, p)    
             # moving left
             self.visit(cells, queue, p.x, p.y-1, p)     
-	        # moving down
+            # moving down
             self.visit(cells, queue, p.x+1, p.y, p)             
-	        #moving right
-            self.visit(cells, queue, p.x, p.y+1, p)
+        
             if len(queue) > 0:
                 p = queue.pop(0)
             else:
                 p = None       
-	    #compose the path if path exists
+        #compose the path if path exists
         if dest == None :
             print("there is no path.")
             return
@@ -115,32 +135,34 @@ class ShortestPathBetweenCellsBFS :
             path = []
             p = dest
             while p != None :
-                path.insert(0, p)	      
-                p = p.prev	       
+                path.insert(0, p)
+                p = p.prev      
             for i in path:
                 print(i)
-	
-	#function to update cell visiting status, Time O(1), Space O(1)
-    def visit(self, cells, queue, x, y, parent) :		
+
+    #function to update cell visiting status, Time O(1), Space O(1)
+    def visit(self, cells, queue, x, y, parent) :
         #out of boundary
         if x < 0 or x >= len(cells) or y < 0 or y >= len(cells[0]) or cells[x][y] == None :
-	        return
-	    #update distance, and previous node
+          return
+       #update distance, and previous node
         dist = parent.dist + 1
         p = cells[x][y]
         if dist < p.dist :
             p.dist = dist
             p.prev = parent
             queue.append(p)
+            
 
-myObj = ShortestPathBetweenCellsBFS()   
-#case1, there is no path
-start = [3, 2]
-end = [4, 4]
-# print(matrix)
-print(matrix[0])
-print("case 1: ")
-myObj.shortestPath(matrix, start, end)
+def calcShortestRoute():
+    myObj = ShortestPathBetweenCellsBFS()   
+    #case1, there is no path
+    start = [3, 3]
+    end = [4, 4]
+    # print(matrix)
+    # print(matrix[0])
+    print("case 1: ")
+    myObj.shortestPath(matrix, start, end)
 
 
 ### 
