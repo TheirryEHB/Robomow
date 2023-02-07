cdb3938bc2b8338c801e2faae6086a1758da7fc6
ThierryKlougbo <80406706+TheirryEHB@users.noreply.github.com>
2023-02-07 18:39:26 +0100

    First draft go point matrix

--- imagerec.py
+++ imagerec.py
@@ -33,6 +33,7 @@ weight = 0
 
 ## Orientation
 currOrientation = 0
+degreesToTurn = 0
 
 
 testHeight = [0,1,2,3,4,5,6]
@@ -45,15 +46,65 @@ def changeOrLeft(degree):
 #     if(currOrientatiion+degree > 180):
 #         currOrientatiion = -90 - degree
     if(currOrientation + degree < 360):
-        currOrientatiion = currOrientatiion + degree
-    if(currOrientatiion + degree >= 360):
-        currOrientatiion = degree - (360 - currOrientatiion)
+        currOrientation = currOrientation + degree
+    if(currOrientation + degree >= 360):
+        currOrientation = degree - (360 - currOrientation)
 def changeOrRight(degree):
-    if(currOrientatiion - degree >= 0):
-        currOrientatiion = currOrientatiion - degree
-    if(currOrientatiion - degree < 0):
-        currOrientatiion = 360 - (degree - currOrientatiion)
+    if(currOrientation - degree >= 0):
+        currOrientation = currOrientation - degree
+    if(currOrientation - degree < 0):
+        currOrientation = 360 - (degree - currOrientation)
+        
+def matrixGoRight():
+    if(currOrientation < 270):
+        degreesToTurn = 270 - currOrientation
+        print("turn left")
+        #turn degrees to left
+    elif(currOrientation >= 270):
+        degreesToTurn = currOrientation - 270
+        print("turn right")
+        #turn degrees to right
+    
+def matrixGoLeft():
+    if(currOrientatiion < 90):
+        degreesToTurn = 90 - currOrientation
+        print("turn right")
+        #turn degrees to right
+    elif(currOrientatiion >= 90):
+        degreesToTurn = currOrientation - 90
+        print("turn left")
+        #turn degrees to left
 
+def matrixGoForward():
+    degreesToTurn = currOrientation
+    print("turn hella left")
+    #turn degrees to left
+    
+def matrixGoBack():
+    if(currOrientatiion < 180):
+        degreesToTurn = 180 - currOrientatiion
+        print("turn left")
+        #turn degrees to left
+    elif(currOrientatiion >= 180):
+        degreesToTurn = currOrientatiion - 180
+        print("turn right")
+        #turn degrees to right
+
+def calcMatrixGoDirection(pointToGo):
+    print(currentPosRob)
+    if(pointToGo[1] == currentPosRob[1]): # turn left or right
+        if(pointToGo[0] > currentPosRob[0]):
+            matrixGoRight()
+        elif(pointToGo[0] < currentPosRob[0]):
+            matrixGoLeft()
+    if(pointToGo[0] == currentPosRob[0]): # go forward or back
+        if(pointToGo[1] > currentPosRob[1]):
+           matrixGoForward()
+        if(pointToGo[1] < currentPosRob[1]):
+            matrixGoBack()
+
+    
+    
 ###
     # Make matrix
 ###
@@ -78,31 +129,29 @@ class Cell  :
         return "("+ str(self.x) + "," + str(self.y) + ")" 
     
 class ShortestPathBetweenCellsBFS :
-    #BFS, Time O(n^2), Space O(n^2)
+     #BFS, Time O(n^2), Space O(n^2)
     def shortestPath(self, matrix, start, end) :
         sx = start[0]
         sy = start[1]
         dx = end[0]
         dy = end[1]
-    #if start or end value is 0, return
+		#if start or end value is 0, return
         if matrix[sx][sy] == 0 or matrix[dx][dy] == 0 :
             print("There is no path.")
             return  
-    #initialize the cells 
+		#initialize the cells 
         m = len(matrix)
         n = len(matrix[0])    
         cells = []
         for i in range (0, m) :
             row = []
-            for j in range(0, n) :
-                row.append(Cell(i, j, sys.maxsize, None))
-                if matrix[i][j] != "*" :
+            for j in range(0, n) :               
+                if matrix[i][j] != 0 :
                     row.append(Cell(i, j, sys.maxsize, None))
                 else:
                     row.append(None)
-#                     row.append(myObj.shortestPath(matrix, [row[-1].x, row[-1].y], end))
             cells.append(row) 
-        #breadth first search
+    #breadth first search
         queue = []     
         src = cells[sx][sy]
         src.dist = 0
@@ -110,24 +159,23 @@ class ShortestPathBetweenCellsBFS :
         dest = None
         p = queue.pop(0)
         while p != None :
-           #find destination 
+        #find destination 
             if p.x == dx and p.y == dy : 
                 dest = p
-                break           
-            #moving right
-            self.visit(cells, queue, p.x, p.y+1, p)
-            # moving up
+                break          
+       # moving up
             self.visit(cells, queue, p.x-1, p.y, p)    
             # moving left
             self.visit(cells, queue, p.x, p.y-1, p)     
-            # moving down
+        # moving down
             self.visit(cells, queue, p.x+1, p.y, p)             
-        
+        #moving right
+            self.visit(cells, queue, p.x, p.y+1, p)
             if len(queue) > 0:
                 p = queue.pop(0)
             else:
                 p = None       
-        #compose the path if path exists
+    #compose the path if path exists
         if dest == None :
             print("there is no path.")
             return
@@ -135,17 +183,18 @@ class ShortestPathBetweenCellsBFS :
             path = []
             p = dest
             while p != None :
-                path.insert(0, p)
-                p = p.prev      
+                path.insert(0, p)     
+                p = p.prev     
             for i in path:
-                print(i)
+                shortestRout.append([i.x, i.y])
+                print(shortestRout)
 
     #function to update cell visiting status, Time O(1), Space O(1)
     def visit(self, cells, queue, x, y, parent) :
         #out of boundary
         if x < 0 or x >= len(cells) or y < 0 or y >= len(cells[0]) or cells[x][y] == None :
-          return
-       #update distance, and previous node
+	        return
+	    #update distance, and previous node
         dist = parent.dist + 1
         p = cells[x][y]
         if dist < p.dist :
@@ -155,15 +204,24 @@ class ShortestPathBetweenCellsBFS :
             
 
 def calcShortestRoute():
+    global currentPosRob
     myObj = ShortestPathBetweenCellsBFS()   
     #case1, there is no path
-    start = [3, 3]
+    start = [3, 2]
+    currentPosRob = [3, 2]
     end = [4, 4]
-    # print(matrix)
+#     print(matrix)
     # print(matrix[0])
     print("case 1: ")
     myObj.shortestPath(matrix, start, end)
+    for i in shortestRout:
+        if(i != currentPosRob):
+            calcMatrixGoDirection(i)
+            currentPosRob = i
+            
+            
 
+calcShortestRoute()
 
 ### 
     # Het centrum van de gevonden shapes vinden.
