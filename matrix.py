cfa0f7b4309146dce130cf1a6f588051ae3e3963
ThierryKlougbo <80406706+TheirryEHB@users.noreply.github.com>
2023-03-04 12:49:20 +0100

    Serial comm met laptop werkt niet

--- matrix.py
+++ matrix.py
@@ -7,53 +7,83 @@ import serial
 import math
 import sys
 import queue
+from enum import Enum
 
+
+# Variables
+toSend = ""
 bigMatrix = []
+angleArray = []
+class Connected(Enum):
+    Connected = 1
+    NotConnected = 2
+connectedState = Connected.NotConnected
 
-#https://avrgeeks.com/connect-arduino-with-computer-using-hc05/#:~:text=Steps%20to%20connect%20to%20the%20Laptop&text=Once%20you%20find%20the%20HC,%E2%80%9Ccomport%E2%80%9D%20of%20the%20device.
-#https://stackoverflow.com/questions/34550437/serialexception-could-not-open-port-access-is-denied
-name = 'HC-05'
-serverMAC = 'E0-D4-64-C1-97-E1'  # mac_bl 98:d3:31:f6:77:ff #mac_pc E0-D4-64-C1-97-E1
-hostMac = '98:d3:31:f6:77:ff'
-port = 11
-passkey = "1234"
+# https://avrgeeks.com/connect-arduino-with-computer-using-hc05/#:~:text=Steps%20to%20connect%20to%20the%20Laptop&text=Once%20you%20find%20the%20HC,%E2%80%9Ccomport%E2%80%9D%20of%20the%20device.
+# https://stackoverflow.com/questions/34550437/serialexception-could-not-open-port-access-is-denied
+# name = 'HC-05'
+# serverMAC = 'E0-D4-64-C1-97-E1'  # mac_bl 98:d3:31:f6:77:ff #mac_pc E0-D4-64-C1-97-E1
+# hostMac = '98:d3:31:f6:77:ff'
+# port = 11
+# passkey = "1234"
 
 COM = "COM5"
-#voor laptop outgoing: COM10(spp dev), incomming COM9
-#laptop usb COM5
-#Voor desktop outgoing: COM11(spp dev), incomming COM10
-BAUD = 9600
-SerialPort = serial.Serial(COM, BAUD, timeout=0.001)
-time.sleep(5)
+# Voor laptop outgoing: COM10(spp dev), incomming COM9
+# laptop usb COM5
+# Voor desktop outgoing: COM11(spp dev), incomming COM10
+BAUD = 115200
+SerialPort = serial.Serial('/dev/ttyACM0',  baudrate=BAUD, timeout=0.1)
+connectedState = Connected.Connected
+# time.sleep(10)
+# SerialPort.write(bytes("2", 'utf-8'))
 
-def sendData(toSend):
+def sendData():
+    global toSend, SerialPort
     SerialPort.close()
     SerialPort.open()
-    SerialPort.write(str(toSend+"\n").encode("utf-8"))
-    readData()
+    toSendString = str(toSend)+"\n"
+    print(toSendString)
+    # SerialPort.write(toSendString.encode("utf-8"))
+    SerialPort.write(bytes(toSendString, 'utf-8'))
+    toSend = ""
+    # SerialPort.close()
 
 def readData():
+    global toSend
     while 1:
+        if toSend != "":
+            sendData()
+        else:
+            try:
+                # SerialPort.open()
+                incomingData = SerialPort.readline()
+                if incomingData:
+                    print(incomingData)
+                    decodedData = incomingData.decode('utf-8')
+                    print(decodedData)
+                    if "Degrees Turned: " in decodedData:
+                        print(decodedData[15:-1])
+                        angleArray.append(decodedData[15:-1])
+                    elif "Done turning." in decodedData:
+                        toSend = str(6)
+                        break
+
+                time.sleep(0.01)
+                # OutgoingData=input('> ')
+                # SerialPort.write(bytes("OutgoingData",'utf-8'))
+                # SerialPort.write(bytes("2",'utf-8'))
+            except KeyboardInterrupt:
+                print("Closing and exiting the program")
+                SerialPort.close()
+                sys.exit(0)
         # SerialPort.close()
-        # SerialPort.open()
-        IncomingData = SerialPort.readline()
-        if IncomingData:
-            print(IncomingData.decode('utf-8'))
 
 
-# sendData(str(2))
-# time.sleep(5)
-# readData()
+if connectedState == Connected.Connected:
+    # global toSend
+    print("Connected")
 
-while (1):
-    try:
-        OutgoingData=input('> ')
-        SerialPort.write(bytes(OutgoingData,'utf-8'))
-    except KeyboardInterrupt:
-        print("Closing and exiting the program")
-        SerialPort.close()
-        sys.exit(0)
-    IncomingData=SerialPort.readline()
-    if(IncomingData):
-        print((IncomingData).decode('utf-8'))
-    time.sleep(0.01)
+    # sendData(str(2))
+    # time.sleep(5)
+    toSend = 2
+    readData()
