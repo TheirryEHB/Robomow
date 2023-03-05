e7736368e4a5f6c6e8fde2dbcb997caaf7d86147
ThierryKlougbo <80406706+TheirryEHB@users.noreply.github.com>
2023-03-05 17:13:37 +0100

    Matrix maken

--- matrix.py
+++ matrix.py
@@ -8,11 +8,12 @@ import math
 import sys
 import queue
 from enum import Enum
+import cv2
 
 
 # Variables
 toSend = ""
-bigMatrix = []
+pingsSend = []
 angleArray = []
 class Connected(Enum):
     Connected = 1
@@ -37,6 +38,107 @@ connectedState = Connected.Connected
 SerialPort.close()
 SerialPort.open()
 time.sleep(2)
+
+
+def makematrix():
+    def degrees2radians(degree):
+        return degree*np.pi/180
+
+    def pinglist2radianslist(pinglist):
+        return [[x[0], degrees2radians(x[1])] for x in pinglist]
+
+
+    # step 4: rescale to quarter wheel turn
+    def rescale(poleCords):
+        poleCords = np.array(poleCords)
+        scale = 57/4  # how many pings go into a 1/4 wheelturn
+        poleCords[:, 0] = poleCords[:, 0]/scale
+        return poleCords
+
+    # step 5: Translate To x,y coordinates [x,y]
+    def pol2cart(rho, phi):
+        x = rho * np.cos(phi)
+        y = rho * np.sin(phi)
+        return(x, y)
+
+    ## lijst van polen naar cartesysche coordinaten brengen
+    def polToCart(poleCords):
+        cartCords = [[0, 0]]
+        for i in range(len(poleCords)):
+            angle = sum([cor[1] for cor in poleCords[:i+1]]) #Som van alle hoeken voor de hoek waar we nu zitten.
+            x, y = pol2cart(poleCords[i][0], angle) # x, y verkrijgen door bereking met poolcoordinaten (amplitude en hoek). Dit vanaf oorsprong perspectief
+            cartCords.append([cartCords[i][0]+x, cartCords[i][1]+y]) # verplaatsen x,y (nieuwe oorsprong) naar coorinaten voorig punt door voorig punt ermee op te tellen 
+        cartCords.append([0, 0])
+        return cartCords
+
+
+    # step 6: shift axis to only positive numbers
+    # Bepalen nullpunt van matrix zodat Y en x as enkel positieve waarden hebben
+    # cartCords = np.array([[0, 0], [30, 0], [20, 10], [30, 15], [5, 15], [10, 5], [
+    #  0, 0]])
+    def shiftAxis(cartCords):
+        cartCords = np.array(np.round(cartCords), dtype=int)
+        x_axis = cartCords[:, 0]
+        y_axis = cartCords[:, 1]
+        # x-axis
+        if any(x_axis < 0):
+            cartCords[:, 0] -= min(x_axis)
+        # y-axis
+        if any(y_axis < 0):
+            cartCords[:, 1] -= min(y_axis)
+        return cartCords
+
+    # step 7: draw to canvas
+    def drawToCanvas(cartCords):
+        canvas = np.zeros((max(cartCords[:, 1])+1, max(cartCords[:, 0])+1))
+        [cv2.line(canvas, cartCords[i].tolist(),
+                  cartCords[i+1].tolist(), 255, 1)for i in range(cartCords.shape[0]-1)]
+        canvas = cv2.rotate(canvas, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        cv2.imwrite("canvas.jpg", canvas)
+        return canvas
+
+    # step 8: fill in box matrix
+    def fillMatrix(matrixEdge):
+        matrixEdge//255
+        print(matrixEdge.shape)
+        matrix = np.zeros_like(matrixEdge)
+        for y in range(1, matrixEdge.shape[0]-1):
+            for x in range(1, matrixEdge.shape[1]-1):
+                if matrixEdge[y, x] != 1:
+                    inside = 0
+                    for i in range(x, -1, -1):
+                        if matrixEdge[y, i] == 1:
+                            inside += 1
+                            break
+                    for i in range(x, matrixEdge.shape[1]):
+                        if matrixEdge[y, i] == 1:
+                            inside += 1
+                            break
+                    for i in range(y, -1, -1):
+                        if matrixEdge[i, x] == 1:
+                            inside += 1
+                            break
+                    for i in range(y, matrixEdge.shape[0]):
+                        if matrixEdge[i, x] == 1:
+                            inside += 1
+                            break
+                    if inside == 4:
+                        matrix[y, x] = 1
+        return matrix*255
+
+
+
+    poleCords = pinglist2radianslist(pingsSend)
+    print(poleCords)
+    poleCords = rescale(poleCords)
+    cartCords = polToCart(poleCords)
+    cartCords = shiftAxis(cartCords)
+    canvas = drawToCanvas(cartCords)
+    matrix = fillMatrix(canvas)
+    cv2.imwrite("filled_matrix.jpg", matrix)
+
+
+
 
 
 def sendData():
